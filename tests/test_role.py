22a497d17a97e361404359f7365e3bf9a5fbc767
John Freeman <john.freeman.opensource@gmail.com>
2016-08-28 19:57:05 +0100

    Implemented Terminator role
    Role to install the Terminator terminal emulator.

--- tests/test_role.py
+++ tests/test_role.py
@@ -3,8 +3,5 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_hosts_file(File):
-    hosts = File('/etc/hosts')
-
-    assert hosts.user == 'root'
-    assert hosts.group == 'root'
+def test_terminator(Command):
+    assert Command('which terminator').rc == 0
