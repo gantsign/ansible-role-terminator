11f1cfe291444ed65e6576c17698f0a3f0ac27d7
John Freeman <john.freeman.opensource@gmail.com>
2018-04-13 17:11:48 +0100

    Updated Molecule to 2.12.1 (#20)
    Keeping up with the latest changes.

--- tests/test_role.py
+++
@@ -1,7 +0,0 @@
-from testinfra.utils.ansible_runner import AnsibleRunner
-
-testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
-
-
-def test_terminator(Command):
-    assert Command('which terminator').rc == 0
